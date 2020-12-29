088d72bd8385de0a013832d793937471e9bb61b3
Typhoon06 <wonhui0222@gmail.com>
2020-12-29 19:59:46 +0900

    Update

---
+++ practice.py
@@ -0,0 +1,17 @@
+# print(5)
+# print(10)
+# print(15)
+# print(20)
+# print(25)
+# print(10+10)
+# print(10-10)
+# print(10*10)
+# print(10/10)
+
+
+
+# print(2*(6+7)/10/2)
+print('풍선')
+print('나비')
+print('ㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
+print('*')
